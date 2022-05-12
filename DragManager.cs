60943cfa6dc3582ea5dca905a2ea2cca4b137707
Rare Coders <105172414+rare-coders@users.noreply.github.com>
2022-05-13 00:02:40 +0530

    Add files via upload
    How to use?
    
    Add this script on the object You want to drag and set following variables :~
    mainCamera - The camera you're using in the scene
    endPointTop - Top Boundary point (can be created empty gameobject )
    endPointBottom - BottomBoundary point (can be created empty gameobject )
    
    For more practical implementation follow the video tutorial :~
    https://youtu.be/zc0_CU-pkRY

---
+++ DragManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DragManager : MonoBehaviour
+{
+    [SerializeField]
+    Camera mainCamera;
+
+    [SerializeField]
+    Transform endPointTop; //Boundry On Top
+
+    [SerializeField]
+    Transform endPointBottom; //Boundry on Bottom;
+
+    Vector3 inputVector;
+    Vector3 unclampedVector;
+    Vector3 clampedVector;
+
+    private void OnMouseDrag()
+    {
+        inputVector = Input.mousePosition;
+        inputVector.z = mainCamera.WorldToScreenPoint(transform.position).z;
+        unclampedVector = mainCamera.ScreenToWorldPoint(inputVector);
+        //We will need to clamp our value to keep object inside the screen
+        ClampValue();
+        transform.position = clampedVector; 
+    }
+
+    void ClampValue()
+    {
+        clampedVector.x = Mathf.Clamp(unclampedVector.x, endPointTop.position.x + 0.5f, endPointBottom.position.x - 0.5f);
+        clampedVector.y = Mathf.Clamp(unclampedVector.y, endPointBottom.position.y + 0.5f, endPointTop.position.y - 0.5f);
+        clampedVector.z = unclampedVector.z;
+    }
+}
